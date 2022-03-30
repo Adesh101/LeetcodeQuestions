07e84b4b56a3b5cda611a048732c79e0380eed8d
Adesh Pingle <48215876+Adesh101@users.noreply.github.com>
2022-03-30 15:55:33 +0530

    Time: 26 ms (35.40%), Space: 61.6 MB (55.25%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -5,10 +5,12 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>(); 
         for(int i=0; i<arr.length-1; i++){
             tempMin = arr[i+1] - arr[i];
-            absMin = Math.min(absMin, tempMin);
-        }
-        for(int i=0; i<arr.length-1; i++){
-            if(arr[i+1]-arr[i]==absMin){
+            if(tempMin<absMin){
+                absMin = tempMin;
+                ans.clear();
+                ans.add(Arrays.asList(arr[i], arr[i+1]));
+            }
+            else if(tempMin==absMin){
                 ans.add(Arrays.asList(arr[i], arr[i+1]));
             }
         }
